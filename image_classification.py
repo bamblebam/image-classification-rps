36d1f144fcd6f8e957d2b12f16df6fb7b61b6007
bamblebam <burhanizrangwala@gmail.com>
2020-11-03 06:03:15 +0530

    implemented earlystopping

--- image_classification.py
+++ image_classification.py
@@ -6,7 +6,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Input, Dropout, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, TensorBoard
 # %%
 train_gen = ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
 gen_train = train_gen.flow_from_directory(
@@ -67,10 +67,16 @@ model.summary()
 reduceLR = ReduceLROnPlateau(
     monitor='val_loss', factor=0.1, patience=2, verbose=0, mode='auto',
     min_delta=0.0001, cooldown=0, min_lr=0)
-callbacks = [reduceLR]
+earlyStop = tf.keras.callbacks.EarlyStopping(
+    monitor='accuracy', min_delta=0.001, patience=10, verbose=0, mode='auto',
+    baseline=None, restore_best_weights=True)
+callbacks = [reduceLR, earlyStop]
 # %%
 model.fit(x=gen_train, epochs=10,
           validation_data=gen_test, callbacks=callbacks)
+# %%
+
+print(model.metrics[0])
 # %%
 model.save('models/model3.h5')
 # %%
